6b91112bc7df4a11f1f6859452afa53d0947e02a
Tom Van den Bon <tvdbon@gmail.com>
2012-02-21 20:12:32 +0200

    first commit

---
+++ coderwall_api/CoderwallAPI.cs
@@ -0,0 +1,91 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CoderwallAPI.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace coderwall_api
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Runtime.Serialization;
+  using System.Web.Script.Serialization;
+  using System.Net;
+  using System.IO;
+  
+  /// <summary>
+  /// TODO: Update summary.
+  /// </summary>
+  public class CoderwallAPI
+  {
+    public struct BadgesStruct
+    {
+      public string Name;
+      public string Description;
+      public string BadgeImage;
+    };
+
+    public string Username;
+    public string Name;
+    public string Location;
+    public int Endorsements;
+    public int BadgeCount;
+    public BadgesStruct[] Badges;
+
+    public CoderwallAPI(string UserName)
+    {
+      string apiCall = "http://coderwall.com/" + UserName + ".json";
+
+      doWebRequest(apiCall);
+    }
+
+    private void doWebRequest(string apicall)
+    {
+      string url = apicall;
+
+      HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(apicall);
+      request.Method = "GET";
+      request.ContentType = "application/json; charset=utf-8";
+
+      HttpWebResponse response;
+
+      try
+      {
+        response = (HttpWebResponse)request.GetResponse();
+      }
+      catch (Exception Ex)
+      {
+        // TODO: correct error handling
+        return;
+      }
+
+      TextReader temp = new StreamReader(response.GetResponseStream());
+      string data = temp.ReadToEnd();
+
+      temp.Close();
+
+      var serializer = new JavaScriptSerializer();
+      serializer.RegisterConverters(new[] { new DynamicJsonConverter() });
+
+      dynamic obj = serializer.Deserialize(data, typeof(object));
+
+      Username = obj.username;
+      Name = obj.name;
+      Location = obj.location;
+      Endorsements= (int)obj.endorsements;
+      BadgeCount = (int)obj.badges.Count;
+
+      Badges = new BadgesStruct[BadgeCount];
+
+      for (int c = 0; c < BadgeCount; c++)
+      {
+        Badges[c].Name = obj.badges[c].name;
+        Badges[c].Description = obj.badges[c].description;
+        Badges[c].BadgeImage = obj.badges[c].badge;
+      }
+    }
+    
+  }
+}
---
+++ coderwall_api/mainFrm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace coderwall_api
+{
+  partial class mainFrm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.usernameTextBox = new System.Windows.Forms.TextBox();
+      this.getButton = new System.Windows.Forms.Button();
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(107, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Coderwall Username:";
+      // 
+      // usernameTextBox
+      // 
+      this.usernameTextBox.Location = new System.Drawing.Point(125, 6);
+      this.usernameTextBox.Name = "usernameTextBox";
+      this.usernameTextBox.Size = new System.Drawing.Size(350, 20);
+      this.usernameTextBox.TabIndex = 1;
+      this.usernameTextBox.Text = "schacon";
+      // 
+      // getButton
+      // 
+      this.getButton.Location = new System.Drawing.Point(481, 4);
+      this.getButton.Name = "getButton";
+      this.getButton.Size = new System.Drawing.Size(75, 23);
+      this.getButton.TabIndex = 2;
+      this.getButton.Text = "Get Achievements";
+      this.getButton.UseVisualStyleBackColor = true;
+      this.getButton.Click += new System.EventHandler(this.getButton_Click);
+      // 
+      // listBox1
+      // 
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Location = new System.Drawing.Point(15, 157);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(842, 355);
+      this.listBox1.TabIndex = 3;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(12, 53);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(38, 13);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Name:";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(12, 79);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(51, 13);
+      this.label3.TabIndex = 5;
+      this.label3.Text = "Location:";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(95, 50);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ReadOnly = true;
+      this.textBox1.Size = new System.Drawing.Size(380, 20);
+      this.textBox1.TabIndex = 6;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(95, 76);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.ReadOnly = true;
+      this.textBox2.Size = new System.Drawing.Size(380, 20);
+      this.textBox2.TabIndex = 7;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(12, 104);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(77, 13);
+      this.label4.TabIndex = 8;
+      this.label4.Text = "Endorsements:";
+      // 
+      // textBox3
+      // 
+      this.textBox3.Location = new System.Drawing.Point(95, 102);
+      this.textBox3.Name = "textBox3";
+      this.textBox3.ReadOnly = true;
+      this.textBox3.Size = new System.Drawing.Size(68, 20);
+      this.textBox3.TabIndex = 9;
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(12, 141);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(43, 13);
+      this.label5.TabIndex = 10;
+      this.label5.Text = "Badges";
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(92, 141);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(13, 13);
+      this.label6.TabIndex = 11;
+      this.label6.Text = "0";
+      // 
+      // mainFrm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(869, 534);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.textBox3);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.listBox1);
+      this.Controls.Add(this.getButton);
+      this.Controls.Add(this.usernameTextBox);
+      this.Controls.Add(this.label1);
+      this.Name = "mainFrm";
+      this.Text = "Coderwall API Test";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox usernameTextBox;
+    private System.Windows.Forms.Button getButton;
+    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label6;
+  }
+}
+
